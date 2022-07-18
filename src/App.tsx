5937f0310b5c3eaca3bd606a1511208029f6a402
denis <ddilianov@intricom.es>
2022-07-18 08:49:11 +0200

    react-signature-pad-wrapper doesn't work the way I want, will try react-signature-canvas instead

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React, { useCallback, useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { FormDynamicArray } from "./components/forms/FormDynamicArray";
 import { Formulario1 } from "./components/forms/Formulario1";
+import { SignaturePadTest } from "./components/forms/SignaturePadTest";
 
 function App() {
-    return <FormDynamicArray />;
+    // return <FormDynamicArray />;
+    return <SignaturePadTest />;
     // return <Formulario1 />;
 }
 
